e7b48ec3e09e8fc8b9e760662ca896254e0a8efa
Alexis <alexiskokamura@gmail.com>
2018-04-30 00:04:17 -1000

    started creating intro

---
+++ src/sections/Intro/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Intro.css';
+
+const Intro = () => {
+  return (
+    <div className="intro">
+      <div className="intro__container">
+        <h1 className="intro__h1">Hi there —</h1>
+        <h1 className="intro__h1">I'm Alexis Okamura. I'm a Full Stack Developer from Honolulu, Hawaii.</h1>
+        <span>Lorem ipsum dolor amet authentic pickled crucifix, four loko poke master cleanse VHS post-ironic shoreditch leggings kombucha fingerstache schlitz.
+        Hella bespoke cloud bread beard, raw denim literally photo booth sriracha viral crucifix chia.</span>
+      </div>
+    </div>
+  );
+}
+
+export default Intro;
