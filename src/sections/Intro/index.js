c2e3bede85085f8295c5472122cddc7b744305e9
Alexis <alexiskokamura@gmail.com>
2018-04-30 19:00:50 -1000

    added media queries and animations to intro

--- src/sections/Intro/index.js
+++ src/sections/Intro/index.js
@@ -5,13 +5,13 @@ const Intro = () => {
   return (
     <div className="intro">
       <div className="intro__container">
-        <h1 className="intro__h1">
-          Hello! I’m Alexis Okamura, a full stack developer passionate
+        <h1>
+          Hello! I’m <strong>Alexis Okamura</strong>, a full stack developer passionate
           about design and building delightful user experiences.
         </h1>
-        <span>Let's talk story 🤙 hello@alexisokamura.com</span>
-        <div className="line" />
       </div>
+      <span>view work</span>
+      <div className="vertical-line" />
     </div>
   );
 }
