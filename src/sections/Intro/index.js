4313723dbb7ddda5f86b326deb3da7d71158ccd1
Alexis <alexiskokamura@gmail.com>
2018-04-30 22:27:24 -1000

    intro section is looking pretty good!

--- src/sections/Intro/index.js
+++ src/sections/Intro/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './Intro.css';
 
+import { Link, Element , Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
+
 const Intro = () => {
   return (
     <div className="intro">
       <div className="intro__container">
         <h1>
-          Hello! I’m <strong>Alexis Okamura</strong>, a full stack developer passionate
-          about design and building delightful user experiences.
+          Hello! I’m <a class="h1--highlight">Alexis Okamura</a>, a full
+          stack developer passionate about design and building delightful
+          user experiences. ✨
         </h1>
       </div>
-      <span>view work</span>
+      <Link to="projects" smooth={true} duration={500}>view work</Link>
       <div className="vertical-line" />
     </div>
   );
