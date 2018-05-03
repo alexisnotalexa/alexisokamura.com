df813b5083dea10de69c759b3ea9bfd764cd3ace
Alexis <alexiskokamura@gmail.com>
2018-05-02 16:16:15 -1000

    added project pictures

--- src/sections/Projects/index.js
+++ src/sections/Projects/index.js
@@ -3,15 +3,17 @@ import './Projects.css';
 
 import Row from '../../components/Row';
 
+import ReactKanban from '../../assets/react-kanban.jpg';
+
 const Projects = () => {
   return (
     <div className="projects">
       <div className="project-container">
-        <div className="project-img">
-          <img src="https://www.alexisokamura.com/assets/react-kanban.jpg" />
+        <div className="img">
+          <img src={ReactKanban} />
         </div>
         <div>
-          <div className="tech-stack">
+          <div className="project-container__stack">
             <span>#React</span>
             <span>#React</span>
             <span>#React</span>
@@ -19,8 +21,8 @@ const Projects = () => {
             <span>#React</span>
             <span>#React</span>
           </div>
-          <div className="project-info">
-          <h1>React Kanban</h1>
+          <div className="project-container__info">
+            <h1>React Kanban</h1>
             <span>Pinterest PBR&B scenester occupy. Mixtape live-edge listicle semiotics, crucifix DIY chartreuse messenger bag. Poutine viral etsy tumeric artisan schlitz roof party gastropub pickled put a bird on it jean shorts succulents kickstarter. Semiotics snackwave aesthetic helvetica readymade wayfarers polaroid pinterest mlkshk tumblr hammock blog venmo man bun live-edge.</span>
           </div>
         </div>
