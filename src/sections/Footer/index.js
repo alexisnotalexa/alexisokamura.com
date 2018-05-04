3248b235c2c0d4517c856ea1ff690d75b7337660
Alexis <alexiskokamura@gmail.com>
2018-05-04 12:10:01 -1000

    done with footer for now

--- src/sections/Footer/index.js
+++ src/sections/Footer/index.js
@@ -4,14 +4,13 @@ import './Footer.css';
 const Footer = () => {
   return (
     <div className="footer align-center">
-      <div className="footer-cont">
+      <div className="footer__container">
         <span>Copyright &copy; 2018 – Alexis Okamura</span>
-        <div className="footer-links">
-          <span>Linkedin</span>
-          <span>Github</span>
-          <span>Codepen</span>
-          <span>Twitter</span>
-          <span>Resume</span>
+        <div className="footer__links">
+          <a href="https://codepen.io/alexisnotalexa" target="_blank" title="CodePen">Codepen</a>
+          <a href="https://github.com/alexisnotalexa" target="_blank" title="GitHub">Github</a>
+          <a href="https://www.linkedin.com/in/alexiskokamura" target="_blank" title="Linkedin">Linkedin</a>
+          <a href="https://twitter.com/alexisnotalexa" target="_blank" title="Twitter">Twitter</a>
         </div>
       </div>
     </div>
