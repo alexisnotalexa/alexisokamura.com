834b16deb4c73f84a461af45564b5bd04c44960c
Alexis <alexiskokamura@gmail.com>
2018-05-04 12:38:35 -1000

    added some content to about section

--- src/sections/Contact/index.js
+++ src/sections/Contact/index.js
@@ -7,8 +7,9 @@ const Contact = () => {
       <div className="contact-container">
         <div className="contact__about">
           <h4>About</h4>
-          <p>Lorem ipsum dolor amet mlkshk sustainable tumeric snackwave lyft pug typewriter palo santo ramps. Lo-fi copper mug swag, post-ironic cold-pressed humblebrag disrupt hammock kinfolk cred kickstarter kombucha pitchfork sartorial.</p>
-          <p>Lorem ipsum dolor amet mlkshk sustainable tumeric snackwave lyft pug typewriter palo santo ramps.</p>
+          <p>If you’re going to have any takeaways after looking through my site, I am Alexis, not Alexa.  I’m a full stack developer living in Honolulu, Hawaii.  More than anything I love being creative and get excited about being able to create products that will visually stun people.</p>
+          <p>When I’m not hacking away at my computer I’m jumping on my next flight to see what else this world has to offer!</p>
+          <p>I’m currently looking for full-time opportunities on the West Coast.</p>
         </div>
         <div className="contact__info">
           <h4>Contact</h4>
