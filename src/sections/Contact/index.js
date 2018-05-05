5b011bc22469d8a104e33a04578297ea853893f4
Alexis <alexiskokamura@gmail.com>
2018-05-04 14:14:23 -1000

    redid footer, adding social links in contact section

--- src/sections/Contact/index.js
+++ src/sections/Contact/index.js
@@ -7,14 +7,19 @@ const Contact = () => {
       <div className="contact-container">
         <div className="contact__about">
           <h4>About</h4>
-          <p>If you’re going to have any takeaways after looking through my site, I am Alexis, not <u>Alexa.</u>  I’m a full stack developer living in Honolulu, Hawaii.  More than anything I love being creative and get excited about being able to create products that will visually stun people.</p>
+          <p>If you’re going to have any takeaways after looking through my site, I'm Alexis, not <u>Alexa</u>.  I’m a full stack developer living in Honolulu, Hawaii.  More than anything I love being creative and get excited about being able to create products that will visually stun people.</p>
           <p>When I’m not hacking away at my computer I’m jumping on my next flight to see what else this world has to offer!</p>
-          <p><strong>I’m currently looking for full-time opportunities on the West Coast.</strong></p>
+          <p>I’m currently looking for full-time opportunities on the West Coast.</p>
+          <button>Contact Me</button><button>View Resume</button>
         </div>
         <div className="contact__info">
           <h4>Contact</h4>
           <p>alexiskokamura@gmail.com</p>
           <p>@alexisnotalexa</p>
+          <h4>Social</h4>
+          <p>Codepen</p>
+          <p>Github</p>
+          <p>Linkedin</p>
         </div>
       </div>
     </div>
--- src/sections/Footer/index.js
+++ src/sections/Footer/index.js
@@ -4,15 +4,7 @@ import './Footer.css';
 const Footer = () => {
   return (
     <div className="footer align-center">
-      <div className="footer__container">
-        <span>Copyright &copy; 2018 – Alexis Okamura</span>
-        <div className="footer__links">
-          <a href="https://codepen.io/alexisnotalexa" target="_blank" title="CodePen">Codepen</a>
-          <a href="https://github.com/alexisnotalexa" target="_blank" title="GitHub">Github</a>
-          <a href="https://www.linkedin.com/in/alexiskokamura" target="_blank" title="Linkedin">Linkedin</a>
-          <a href="https://twitter.com/alexisnotalexa" target="_blank" title="Twitter">Twitter</a>
-        </div>
-      </div>
+      <span>Copyright &copy; 2018 – Alexis Okamura</span>
     </div>
   );
 }
