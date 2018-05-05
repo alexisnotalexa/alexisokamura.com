4167059cac69971c5c2acecada33a3f968f932eb
Alexis <alexiskokamura@gmail.com>
2018-05-04 23:02:03 -1000

    more text edits on contact section

--- src/sections/Contact/index.js
+++ src/sections/Contact/index.js
@@ -10,16 +10,26 @@ const Contact = () => {
           <p>If you’re going to have any takeaways after looking through my site, I'm Alexis, not <u>Alexa</u>.  I’m a full stack developer living in Honolulu, Hawaii.  More than anything I love being creative and get excited about being able to create products that will visually stun people.</p>
           <p>When I’m not hacking away at my computer I’m jumping on my next flight to see what else this world has to offer!</p>
           <p>I’m currently looking for full-time opportunities on the West Coast.</p>
-          <button>Contact Me</button><button>View Resume</button>
+          <a href="mailto:alexiskokamura@gmail.com"><button>Contact Me</button></a><a href="" target="_blank"><button>View Resume</button></a>
         </div>
         <div className="contact__info">
           <h4>Contact</h4>
-          <p>alexiskokamura@gmail.com</p>
-          <p>@alexisnotalexa</p>
+          <a href="mailto:alexiskokamura@gmail.com" target="_blank" title="Email">
+            <p>alexiskokamura@gmail.com</p>
+          </a>
+          <a href="https://twitter.com/alexisnotalexa" target="_blank" title="Twitter">
+            <p>@alexisnotalexa</p>
+          </a>
           <h4>Social</h4>
-          <p>Codepen</p>
-          <p>Github</p>
-          <p>Linkedin</p>
+          <a href="https://codepen.io/alexisnotalexa" target="_blank" title="CodePen">
+            <p>Codepen</p>
+          </a>
+          <a href="https://github.com/alexisnotalexa" target="_blank" title="GitHub">
+            <p>Github</p>
+          </a>
+          <a href="https://www.linkedin.com/in/alexiskokamura" target="_blank" title="Linkedin">
+            <p>Linkedin</p>
+          </a>
         </div>
       </div>
     </div>
