6e995d8d09b7552dccadbec4e1827288617fd28d
Alexis <alexiskokamura@gmail.com>
2018-05-08 02:29:39 -1000

    got rid of old project example from projects section, finalized project component

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import './styles.css';
 
-const Project = ({ title, description, img, color, stack }) => {
+const Project = ({ title, description, img, link, color, stack }) => {
   let techStack = stack.map(tech => {
     return (<span>#{tech}</span>)
   });
   return (
     <div className="project-container">
-      <div className="img" style={{background: color}}>
-        <img src={img} />
-      </div>
+      <a href={link} target="_blank">
+        <div className="img-container" style={{background: color}}>
+          <img src={img} />
+        </div>
+      </a>
       <div className="project-container__stack">
         {techStack}
       </div>
       <div className="project-container__info">
-        <h1>{title}</h1>
+        <a href={link} target="_blank"><h1>{title}</h1></a>
         <span>{description}</span>
       </div>
     </div>
--- src/sections/Projects/index.js
+++ src/sections/Projects/index.js
@@ -9,27 +9,11 @@ import ReactKanban from '../../assets/react-kanban.jpg';
 const Projects = () => {
   return (
     <div className="projects align-center">
-      <div className="project-container">
-        <div className="img">
-          <img src={ReactKanban} />
-        </div>
-          <div className="project-container__stack">
-            <span>#React</span>
-            <span>#React</span>
-            <span>#React</span>
-            <span>#React</span>
-            <span>#React</span>
-            <span>#React</span>
-          </div>
-          <div className="project-container__info">
-            <h1>React Kanban</h1>
-            <span>Pinterest PBR&B scenester occupy. Mixtape live-edge listicle semiotics, crucifix DIY chartreuse messenger bag. Poutine viral etsy tumeric artisan schlitz roof party gastropub pickled put a bird on it jean shorts succulents kickstarter. Semiotics snackwave aesthetic helvetica readymade wayfarers polaroid pinterest mlkshk tumblr hammock blog venmo man bun live-edge.</span>
-          </div>
-      </div>
       <Project
         title={'Test'}
         description={'fjldksjfdslkfjalfkjs'}
         img={ReactKanban}
+        link="https://codepen.io/"
         color={'midnightblue'}
         stack={['react', 'react', 'react']}
       />
