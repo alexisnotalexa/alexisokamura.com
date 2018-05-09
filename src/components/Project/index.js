fd7b8c16bf884f884662be3558daee0e949fb5cd
Alexis <alexiskokamura@gmail.com>
2018-05-09 00:05:55 -1000

    removed old styles from projects, added animations to project component

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,27 +1,32 @@
 import React from 'react';
 import './styles.css';
 
+import { Reveal } from 'react-reveal';
+
 const Project = ({ title, description, img, link, color, stack }) => {
   let techStack = stack.map(tech => {
     return (<span>#{tech}</span>)
   });
+
   return (
-    <div className="project-container">
-      <a href={link} target="_blank" rel="noopener noreferrer">
-        <div className="img-container" style={{background: color}}>
-          <img src={img} alt={title} />
-        </div>
-      </a>
-      <div className="project-container__stack">
-        {techStack}
-      </div>
-      <div className="project-container__info">
+    <Reveal duration={2000} effect="fade-in">
+      <div className="project-container">
         <a href={link} target="_blank" rel="noopener noreferrer">
-          <h1>{title}</h1>
+          <div className="img-container" style={{background: color}}>
+            <img src={img} alt={title} />
+          </div>
         </a>
-        <span>{description}</span>
+        <div className="project-container__stack">
+          {techStack}
+        </div>
+        <div className="project-container__info">
+          <a href={link} target="_blank" rel="noopener noreferrer">
+            <h1>{title}</h1>
+          </a>
+          <span>{description}</span>
+        </div>
       </div>
-    </div>
+    </Reveal>
   );
 }
 
