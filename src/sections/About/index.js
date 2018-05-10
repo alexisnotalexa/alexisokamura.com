bd80ffb28241b7f10c4dbb97b85b6542d0f3bd33
Alexis <alexiskokamura@gmail.com>
2018-05-09 14:26:05 -1000

    minor edit

--- src/sections/About/index.js
+++ src/sections/About/index.js
@@ -16,7 +16,7 @@ const About = () => {
           <img src={me} alt="Alexis Okamura" />
         </div>
         <div className="about-info">
-          <span>Nice to meet you! – <span role="img" aria-label="wave">👋</span></span>
+          <strong><span>Nice to meet you! – <span role="img" aria-label="wave">👋</span></span></strong>
           <p>Since I could hold a pencil I absolutely loved art.  As I grew older my passion for art translated into programming.  I use my eye for design and my obsessive attention to detail to create pixel-perfect interfaces.  More than anything I love being creative and get excited about being able to create products that will visually stun people.</p>
           <p>When I’m not hacking away at my computer I’m jumping on my next flight to see what else this world has to offer!</p>
           <p>I’m currently looking for full-time opportunities on the West Coast!  Feel free to <u><a href="mailto:alexiskokamura@gmail.com">email me</a></u> or take a look at my <u><a href={resume} target="_blank">resume</a></u>.</p>
