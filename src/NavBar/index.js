0246c8dbf09a1e5d171baf1f838365ac687b4318
Alexis Okamura <alexiskokamura@gmail.com>
2023-02-19 18:09:50 -0500

    add initial styles for desktop navbar

--- src/NavBar/index.js
+++ src/NavBar/index.js
@@ -1,11 +1,14 @@
 import { Link } from 'react-router-dom';
+import './NavBar.css';
 
 const NavBar = () => {
     return (
-        <nav>
-            <Link to={'/'}>ao</Link>
-            <Link to={'/about'}>About</Link>
-            <Link to={'/'}>Resume</Link>
+        <nav className='navbar'>
+            <Link className='navbar__logo' to={'/'}>ao</Link>
+            <div className='navbar__links'>
+                <Link to={'/about'}>About</Link>
+                <Link to={'/'}>Resume</Link>
+            </div>
         </nav>
     );
 };
