e306c733cb7265bfb5d318a40777a5445348d53a
Alexis Okamura <alexiskokamura@gmail.com>
2023-02-21 22:54:04 -0500

    get rid of mobile navbar styling

--- src/NavBar/index.js
+++ src/NavBar/index.js
@@ -1,8 +1,6 @@
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
-import './NavBar.css';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './NavBar.css';
 
 const NavBar = () => {
     const [isDarkMode, setIsDarkMode] = useState(false);
@@ -11,15 +9,11 @@ const NavBar = () => {
         <button className='navbar__theme-btn' onClick={() => setIsDarkMode(!isDarkMode)}>
             {isDarkMode ? '🌚' : '🌞'}
         </button>
-    )
+    );
     
     return (
         <nav className='navbar'>
             <Link className='navbar__logo' to={'/'}>ao</Link>
-            <div className='navbar__mobile-menu'>
-                {renderThemeIcon()}
-                <FontAwesomeIcon icon={faBars} />
-            </div>
             <div className='navbar__links'>
                 {renderThemeIcon()}
                 <Link to={'/about'}>About</Link>
