5786f2a4c126eabf206062c5ad4dd34b4dfba6af
Alexis Okamura <alexiskokamura@gmail.com>
2023-02-23 11:07:38 -0500

    cleaning up styles

--- src/Home/index.js
+++ src/Home/index.js
@@ -3,18 +3,25 @@ import NavBar from 'NavBar';
 import './Home.css';
 
 const Home = () => {
+    const renderGradientBlob = () => (
+        <div className='home__gradient'>
+            <div className='home__gradient__color' />
+            <div className='home__gradient__color' />
+            <div className='home__gradient__color' />
+            <div className='home__gradient__color' />
+            <div className='home__gradient__backdrop' />
+        </div>
+    );
+
     return (
         <div className='home'>
             <NavBar />
-            <div className='home__content'>
-                <h1>Hi, I'm Alexis, not <i>Alexa</i>.<br />I'm a Software Engineer based in Brooklyn, NY.</h1>
-            </div>
-            <div className='home__gradient'>
-                <div className='home__gradient__color' />
-                <div className='home__gradient__color' />
-                <div className='home__gradient__color' />
-                <div className='home__gradient__color' />
-                <div className='home__gradient__backdrop' />
+            <div className='home__content-container'>
+                <div className='home__content'>
+                    <h1>Hi, I'm Alexis, not <i>Alexa</i>.<br />I'm a Software Engineer based in Brooklyn, NY.</h1>
+                    <h3>Blah blah blah</h3>
+                    {renderGradientBlob()}
+                </div>
             </div>
             <Footer />
         </div>
