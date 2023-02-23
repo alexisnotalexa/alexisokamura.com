eaa7f8dd165ce5b53700bf6f730993f1125994a7
Alexis Okamura <alexiskokamura@gmail.com>
2023-02-23 18:25:14 -0500

    tweak footer styling

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,17 +1,20 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithubAlt, faLinkedinIn, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faGithubAlt, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import './Footer.css';
 
 const Footer = () => {
     const year = new Date().getFullYear();
+
+    const renderSocialLinks = () => 
+        [faLinkedinIn, faGithubAlt, faEnvelope].map((icon) => <FontAwesomeIcon className='footer__icon' icon={icon} />);
+
     return (
         <footer>
             <p>{year} &copy; Alexis Okamura</p>
-            <hr />
-            <FontAwesomeIcon icon={faLinkedinIn} />
-            <FontAwesomeIcon icon={faGithubAlt} />
-            <FontAwesomeIcon icon={faEnvelope} />
+            <div>
+                {renderSocialLinks()}
+            </div>
         </footer>
     )
 };
