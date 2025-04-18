3d80457553cc366e1d6013c441d6e729f0f31ef9
Alexis Okamura <alexiskokamura@gmail.com>
2025-04-17 20:51:24 -0400

    content updates

--- app/main/main.tsx
+++ app/main/main.tsx
@@ -16,42 +16,25 @@ export function Main() {
 }
 
 function About() {
-    function renderTest() {
-        return (
-            <>
-                <img className="w-3/4 mb-6 sm:w-1/2 grayscale hover:grayscale-0 transition-all duration-1000" src={profilePic} />
-                <div className="flex-1 w-full">
-                    <p className="mb-6">Born and raised in Honolulu, I've lived coast to coast—Seattle and now Brooklyn, where I live with my cat, Frederick Winston. Currently, I'm a Software Engineer at Remitly, focused on building features that help people worldwide send money to their loved ones.</p>
-                    <p>My love for art and design began as soon as I could hold a pencil—and it's shaped everything I do. As a software engineer, I bring a sharp eye and obsessive attention to detail to crafting interfaces that are both visually striking and thoughtfully engineered. Art has always been at the core of who I am, and it's what drives my passion for building beautiful, meaningful digital experiences.
-                    </p>
-                </div>
-            </>
-        );
-    }
-
-    function renderTest2() {
-        return (
-            <div className="flex flex-col sm:flex-row w-full items-center">
-                <img className="w-2/5 pr-8" src={profilePic} />
-                <div className="flex-1 w-1/2">
-                    <p>Born and raised in Honolulu, I've lived coast to coast—Seattle and now Brooklyn, where I live with my cat, Frederick Winston. Currently, I'm a Software Engineer at Remitly, focused on building features that help people worldwide send money to their loved ones.</p>
-                    <p>My love for art and design began as soon as I could hold a pencil—and it's shaped everything I do. As a software engineer, I bring a sharp eye and obsessive attention to detail to crafting interfaces that are both visually striking and thoughtfully engineered. Art has always been at the core of who I am, and it's what drives my passion for building beautiful, meaningful digital experiences.
-                    </p>
-                </div>
-            </div>
-        );
-    }
-
     return (
         <div className="flex flex-col items-center text-center">
-            <h1 className="font-medium text-3xl mb-10 sm:text-5xl sm:mb-12">alexis okamura</h1>
-            {renderTest()}
+            <h1 className="font-semibold text-3xl mb-10 sm:mb-12">alexis okamura</h1>
+            <div className="flex flex-col w-full items-center md:flex-row">
+                <img className="w-3/4 mb-10 md:mb-0 md:w-2/5 md:pr-8" src={profilePic} />
+                <div className="w-full flex-1 md:w-1/2">
+                    <p className="mb-4">Born and raised in Honolulu, I've lived coast to coast—Seattle and now Brooklyn, where I live with my cat, Frederick Winston. Currently, I'm a Software Engineer at Remitly, focused on building features that help people worldwide send money to their loved ones.</p>
+                    <p className="mb-4">My love for art and design began as soon as I could hold a pencil. As a software engineer, I bring a sharp eye and obsessive attention to detail to crafting interfaces that are both visually striking and thoughtfully engineered. Art has always been at the core of who I am, and it's what drives my passion for building beautiful, meaningful digital experiences.
+                    </p>
+                    <p>✽</p>
+                </div>
+            </div>
         </div>
     );
 }
 
 interface Experience {
     title: string;
+    href: string;
     duration: string;
     description: string;
     technologies: Array<string>;
@@ -61,40 +44,49 @@ function WorkExperience() {
     const experiences: Array<Experience> = [
         {
             title: "Software Engineer @ Remitly",
-            duration: "'22-Now",
+            href: "https://www.remitly.com/",
+            duration: "11/22—Now",
             description: "I contributed to various teams, most recently focusing on streamlining the send experience for users transferring money to themselves. I also played a key role in helping to migrate existing design tokens to our new design token system, helping to establish a more cohesive and scalable foundation across the platform.",
             technologies: ["React Native", "TypeScript", "Redux", "Apollo GraphQL", "GraphQL", "Node.js"]
         },
         {
             title: "Software Engineer @ Compass",
-            duration: "21'-22'",
+            href: "https://www.compass.com/",
+            duration: "12/21—9/22",
             description: "I designed and launched an offer management tool that streamlined how agents present client offers. Additionally, I spearheaded the transition to our in-house design system, improving UI consistency and accelerating development cycles.",
             technologies: ["React", "TypeScript", "Redux", "MobX"]
         },
         {
             title: "Software Engineer @ Cisco",
-            duration: "19'-21'",
+            href: "https://www.cisco.com/c/en/us/products/conferencing/webex.html",
+            duration: "10/19—11/21",
             description: "I helped modernize legacy contact center software by building scalable, reusable UI components. I also established front-end coding standards and significantly increased test coverage, enhancing code quality and team alignment.",
             technologies: ["React", "TypeScript", "Apollo GraphQL", "GraphQL", "Web Components", "Scala"]
         },
         {
             title: "Front-End Software Engineer @ Flyhomes",
-            duration: "18'-19'",
+            href: "https://flyhomes.com/",
+            duration: "06/18—10/19",
             description: "I led the rebranding of our customer-facing website and built a referral program to track user referrals and bonus payouts. I also improved lead conversion by refining the user signup flow and automating the scheduling of introductory calls.",
             technologies: ["React", "Redux", "Ruby on Rails", "AWS"]
         },
     ];
 
-    function renderExperience(experience: Experience) {
+    function renderExperience({ title, href, duration, description, technologies }: Experience) {
         return (
-            <div className="mb-8" role="listitem">
-                <p className="font-medium mb-1">
-                    {experience.title}
-                    <sup className="ml-1">{experience.duration}</sup>
-                </p>
-                <p className="mb-2">{experience.description}</p>
+            <div className="mb-10" role="listitem">
+                <a
+                    href={href}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="group mb-0 transition-all duration-300 relative inline-block"
+                >
+                    <span className="font-medium inline-block after:block after:h-0.5 after:w-full after:scale-x-0 after:bg-black after:transition-transform after:duration-300 after:origin-left group-hover:after:scale-x-100">{title}</span>
+                    <sup className="font-medium ml-1">{duration}</sup>
+                </a>
+                <p className="mb-4">{description}</p>
                 <p className="text-xs" aria-label="Technologies used">
-                    {experience.technologies.map((tech, i, arr) => (
+                    {technologies.map((tech, i, arr) => (
                         <span key={tech}>
                         {tech}
                         {i < arr.length - 1 && <span aria-hidden="true"> • </span>}
@@ -106,13 +98,23 @@ function WorkExperience() {
     }
 
     return (
-        <div className="flex flex-col sm:flex-row">
-            <div className="w-full mb-2 sm:mb-0 sm:text-right sm:pr-6 md:pr-8 lg:pr-12 sm:w-1/4">
-            <h1 className="font-medium text-3xl">experience</h1>
+        <div className="flex flex-col md:flex-row">
+            <div className="w-full mb-2 md:mb-0 md:text-right md:w-1/4 md:pr-8 lg:pr-12">
+            <h1 className="font-semibold text-3xl">experience</h1>
             </div>
             <div className="flex-1 w-full" role="list">
                 {experiences.map((experience) => renderExperience(experience))}
-                <p>Resume →</p>
+                <a
+                  href="https://docs.google.com/document/d/1_51J00ObtTVf8BIMcWZuNjnIhfuLikkv0iPoSgQxRP4/edit?usp=sharing"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="group mb-0 transition-all duration-300 relative"
+                >
+                    <span className="font-medium inline-block after:block after:h-0.5 after:w-full after:scale-x-0 after:bg-black after:transition-transform after:duration-300 after:origin-left group-hover:after:scale-x-100">
+                        Resume
+                    </span>
+                    <span className="font-medium inline-block transition-transform duration-300 group-hover:translate-x-1">&nbsp;→</span>
+                </a>
             </div>
         </div>
     );
@@ -162,19 +164,19 @@ function Contact() {
     const [hovered, setHovered] = useState<"twitter" | "email" | null>(null);
 
     const links = [
-        { label: 'codepen', href: '#' },
-        { label: 'github', href: '#' },
-        { label: 'instagram', href: '#' },
-        { label: 'linkedin', href: '#' },
+        { label: 'codepen', href: 'https://codepen.io/alexisnotalexa' },
+        { label: 'github', href: 'https://github.com/alexisnotalexa' },
+        { label: 'instagram', href: 'https://www.instagram.com/hikkkkki/' },
+        { label: 'linkedin', href: 'https://www.linkedin.com/in/alexiskokamura/' },
     ];
   
     return (
-      <div className="flex flex-col sm:flex-row mb-12" role="list">
+      <div className="flex flex-col md:flex-row" role="list">
         {hovered === "twitter" && <IMessageBubble text="tweet me" />}
         {hovered === "email" && <IMessageBubble text="email me" />}
 
-        <div className="w-full mb-2 sm:mb-0 sm:text-right sm:pr-6 md:pr-8 lg:pr-12 sm:w-1/4">
-          <h2 className="font-medium text-3xl">contact</h2>
+        <div className="w-full mb-2 md:mb-0 md:text-right md:w-1/4 md:pr-8 lg:pr-12">
+          <h2 className="font-semibold text-3xl">contact</h2>
         </div>
   
         <div className="flex flex-col flex-1 w-full space-y-2">
@@ -206,7 +208,7 @@ function Contact() {
                   href={href}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className=" mb-0 inline-block transition-all duration-300 relative after:block after:h-0.5 after:w-full after:scale-x-0 after:bg-black after:transition-transform after:duration-300 after:origin-left hover:after:scale-x-100 active:translate-y-0.5"
+                  className="mb-0 inline-block transition-all duration-300 relative after:block after:h-0.5 after:w-full after:scale-x-0 after:bg-black after:transition-transform after:duration-300 after:origin-left hover:after:scale-x-100"
                 >
                   {label}
                 </a>
@@ -238,21 +240,3 @@ function Footer() {
         </footer>
     );
 }
-
-// function Contact() {
-//     return (
-//         <div className="flex flex-col sm:flex-row">
-//             <div className="w-full mb-2 sm:mb-0 sm:text-right sm:pr-6 md:pr-8 lg:pr-12 sm:w-1/4">
-//             <h1 className="font-medium text-3xl">contact</h1>
-//             </div>
-//             <div className="flex-1 w-full">
-//                 <p>@alexisnotalexa</p>
-//                 <p className="mb-6">alexiskokamura@gmail.com</p>
-//                 <p>github</p>
-//                 <p>linkedin</p>
-//                 <p>codepen</p>
-//                 <p>instagram</p>
-//             </div>
-//         </div>
-//     );
-// }
