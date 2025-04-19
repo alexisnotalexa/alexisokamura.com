1983ec111d02a9a56fa09def5751ccd4eab34ef2
Alexis Okamura <alexiskokamura@gmail.com>
2025-04-18 22:02:46 -0400

    update favicon

--- app/root.tsx
+++ app/root.tsx
@@ -21,6 +21,22 @@ export const links: Route.LinksFunction = () => [
     rel: 'stylesheet',
     href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
   },
+  // Favicon & icons
+  { rel: 'icon', href: '/favicon.ico' },
+  {
+    rel: 'icon',
+    type: 'image/png',
+    sizes: '16x16',
+    href: '/favicon-16x16.png',
+  },
+  {
+    rel: 'icon',
+    type: 'image/png',
+    sizes: '32x32',
+    href: '/favicon-32x32.png',
+  },
+  { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+  { rel: 'manifest', href: '/site.webmanifest' },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
