5d84b06b6b3aba03ef7138c0f029674898d4fb83
Alexis Okamura <alexiskokamura@gmail.com>
2025-04-18 21:56:18 -0400

    update meta tags, add google analytics

--- app/root.tsx
+++ app/root.tsx
@@ -5,21 +5,21 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "react-router";
+} from 'react-router';
 
-import type { Route } from "./+types/root";
-import "./app.css";
+import type { Route } from './+types/root';
+import './app.css';
 
 export const links: Route.LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
+  { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
   {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
+    rel: 'preconnect',
+    href: 'https://fonts.gstatic.com',
+    crossOrigin: 'anonymous',
   },
   {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
   },
 ];
 
@@ -31,6 +31,20 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=UA-116340731-1"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'UA-116340731-1');
+            `,
+          }}
+        />
       </head>
       <body>
         {children}
@@ -46,15 +60,15 @@ export default function App() {
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
-  let message = "Oops!";
-  let details = "An unexpected error occurred.";
+  let message = 'Oops!';
+  let details = 'An unexpected error occurred.';
   let stack: string | undefined;
 
   if (isRouteErrorResponse(error)) {
-    message = error.status === 404 ? "404" : "Error";
+    message = error.status === 404 ? '404' : 'Error';
     details =
       error.status === 404
-        ? "The requested page could not be found."
+        ? 'The requested page could not be found.'
         : error.statusText || details;
   } else if (import.meta.env.DEV && error && error instanceof Error) {
     details = error.message;
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,11 +1,27 @@
-import type { Route } from "./+types/home";
-// import { Welcome } from "../welcome/welcome";
-import { Main } from "../main/main";
+import type { Route } from './+types/home';
+import { Main } from '../main/main';
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    {
+      name: 'keywords',
+      content:
+        'alexisnotalexa, alexis, okamura, javascript, design, honolulu, new york, seattle, brooklyn',
+    },
+    { name: 'title', content: 'Alexis Okamura | Software Engineer' },
+    {
+      name: 'description',
+      content:
+        'Brooklyn-based software engineer blending a lifelong passion for art and design with precision-engineered, user-focused digital experiences',
+    },
+    { name: 'twitter:site', content: '@alexisnotalexa' },
+    { name: 'twitter:creator', content: '@alexisnotalexa' },
+    { name: 'twitter:title', content: 'Alexis Okamura | Software Engineer' },
+    {
+      name: 'twitter:description',
+      content:
+        'Brooklyn-based software engineer blending a lifelong passion for art and design with precision-engineered, user-focused digital experiences',
+    },
   ];
 }
 
